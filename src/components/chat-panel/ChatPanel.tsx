2ae442dcfb0d920f7dee3f506c7dbcd458e32b5b
Emdadul Islam <bekon1700@gmail.com>
2023-05-09 22:51:28 +0600

    chat panel top section done

--- src/components/chat-panel/ChatPanel.tsx
+++ src/components/chat-panel/ChatPanel.tsx
@@ -1,8 +1,20 @@
+import {RiPhoneLine, RiVideoAddLine} from 'react-icons/ri'
+import {BsThreeDotsVertical} from 'react-icons/bs'
 
 export default function ChatPanel() {
   return (
     <div className="chat-panel">
-      
+      <div className="chat-panel__top">
+        <div className='user'>
+          <p className='img'>R</p>
+          <p className='name'>Ratul Sikder</p>
+        </div>
+        <div className='options'>
+          <RiPhoneLine className="icon"/>
+          <RiVideoAddLine className="icon"/>
+          <BsThreeDotsVertical className="icon"/>
+        </div>
+      </div>
     </div>
   )
 }
